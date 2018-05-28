ba65f89dce3871d3d36aec2d55a73666c5d9e78b
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-28 03:47:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   end
 
   def self.create(student:)
-    Student.new
+    student = Student.new(name, grade)
     sql = <<-SQL
     INSERT INTO students (id, name, grade) VALUES (?, "#{@name}", "#{@grade}")
     SQL
