e95f39f56a5ccfbccebf8a3c5776fd8f3af39085
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-28 03:13:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,16 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql = <<-sql
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    album TEXT
+    )
+    sql
+    DB[:conn].execute(sql)
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
