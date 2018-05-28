81f9dbc9f64515ce64695e2c7f9511e289284adc
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-28 03:33:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
   end
 
   def self.drop_table
-    
+
     sql = <<-SQL
     DROP TABLE students
     SQL
@@ -29,7 +29,13 @@ class Student
   end
 
   def save
+    
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
 
   end
-  
+
 end
